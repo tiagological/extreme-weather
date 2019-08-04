966750e1fff74c6ce0be74411a60d1ed7030cffa
Tiago <t.marques999@gmail.com>
2019-08-04 22:51:55 +0100

    Refactor front end GraphQL client component

--- client/src/components/GraphQLApp.js
+++ client/src/components/GraphQLApp.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import Countries from './Countries';
 import styled from 'styled-components/macro';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql'
 });
+
+client
+  .query({
+    query: gql`
+      query GetCityTemp {
+        cityTemperature(countryCode: "GB", cityName: "London") {
+          Temperature {
+            Metric {
+              Value
+            }
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result.data));
 
 class GraphQLApp extends React.Component {
   render() {
--- schema.js
+++ schema.js
@@ -6,6 +6,7 @@ const {
   GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
+  GraphQLFloat,
   GraphQLList,
   GraphQLSchema
 } = require('graphql');
@@ -17,6 +18,43 @@ const CountryType = new GraphQLObjectType({
     name: { type: GraphQLString },
     alpha2Code: { type: GraphQLString },
     capital: { type: GraphQLString }
+  })
+});
+
+// City Type
+
+const CityKeyType = new GraphQLObjectType({
+  name: 'CityKey',
+  fields: () => ({
+    Key: { type: GraphQLInt }
+  })
+});
+
+// Metric Temp Type
+
+const MetricTempType = new GraphQLObjectType({
+  name: 'MetricTemp',
+  fields: () => ({
+    Value: { type: GraphQLFloat },
+    Unit: { type: GraphQLString }
+  })
+});
+
+// Temp Type
+
+const TempType = new GraphQLObjectType({
+  name: 'Temperature',
+  fields: () => ({
+    Metric: { type: MetricTempType }
+  })
+});
+
+// CityTemp Type
+
+const CityTempType = new GraphQLObjectType({
+  name: 'CityTemp',
+  fields: () => ({
+    Temperature: { type: TempType }
   })
 });
 
@@ -33,6 +71,60 @@ const RootQuery = new GraphQLObjectType({
           headers: {
             'X-RapidAPI-Host': process.env.COUNTRIES_API_HOST,
             'X-RapidAPI-Key': process.env.COUNTRIES_API_KEY
+          }
+        }).then(res => res.data);
+      }
+    },
+    cityTemperature: {
+      type: new GraphQLList(CityTempType),
+      args: {
+        countryCode: { type: GraphQLString },
+        cityName: { type: GraphQLString }
+      },
+      async resolve(parent, args) {
+        const firstResponse = await axios({
+          method: 'get',
+          url: `http://dataservice.accuweather.com/locations/v1/cities/${
+            args.countryCode
+          }/search`,
+          params: {
+            apikey: process.env.ACCUWEATHER_API_KEY,
+            q: args.cityName,
+            language: 'en-gb',
+            details: true
+          }
+        });
+
+        const locationKey = firstResponse.data[0].Key;
+
+        const secondResponse = await axios({
+          method: 'get',
+          url: `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`,
+          params: {
+            apikey: process.env.ACCUWEATHER_API_KEY,
+            language: 'en-gb',
+            details: false
+          }
+        });
+
+        return secondResponse.data;
+      }
+    },
+    cityWeather: {
+      type: new GraphQLList(CityTempType),
+      args: {
+        locationKey: { type: GraphQLInt }
+      },
+      resolve(parents, args) {
+        return axios({
+          method: 'get',
+          url: `http://dataservice.accuweather.com/currentconditions/v1/${
+            args.locationKey
+          }`,
+          params: {
+            apikey: process.env.ACCUWEATHER_API_KEY,
+            language: 'en-gb',
+            details: false
           }
         }).then(res => res.data);
       }
