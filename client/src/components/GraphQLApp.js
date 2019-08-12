198cdeaedf9cb2de3eccbabb0c118682d46a847e
Tiago <t.marques999@gmail.com>
2019-08-12 09:22:37 +0100

    Removes Accuweather API and adds Openweather API

--- client/src/components/GraphQLApp.js
+++ client/src/components/GraphQLApp.js
@@ -4,28 +4,81 @@ import { ApolloProvider } from 'react-apollo';
 import gql from 'graphql-tag';
 import Countries from './Countries';
 import styled from 'styled-components/macro';
+import axios from 'axios';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql'
 });
 
-client
-  .query({
-    query: gql`
-      query GetCityTemp {
-        cityTemperature(countryCode: "GB", cityName: "London") {
-          Temperature {
-            Metric {
-              Value
-            }
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result.data));
+// client
+//   .query({
+//     query: gql`
+//       query GetCitiesTemp {
+//         citiesWeather {
+//           countryName
+//           capital
+//           currentTemp
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err));
 
 class GraphQLApp extends React.Component {
+  componentDidMount = async () => {
+    try {
+      const countryListResponse = await axios({
+        method: 'get',
+        url: 'https://***REMOVED***/rest/v1/all',
+        headers: {
+          'X-RapidAPI-Host': '***REMOVED***',
+          'X-RapidAPI-Key': '***REMOVED***'
+        }
+      });
+
+      const countryList = countryListResponse.data;
+
+      const currentCitiesTempArray = Promise.all(
+        countryList.map(async country => {
+          try {
+            const cityDataResponse = await axios({
+              method: 'get',
+              url: 'https://***REMOVED***/weather',
+              headers: {
+                'X-RapidAPI-Host': '***REMOVED***',
+                'X-RapidAPI-Key':
+                  '***REMOVED***'
+              },
+              params: {
+                q: `${country.capital},${country.alpha2Code}`,
+                units: 'metric'
+              }
+            });
+
+            const cityTempMetric = cityDataResponse.data.main.temp;
+
+            return {
+              countryName: country.name,
+              capital: country.capital,
+              currentTemp: cityTempMetric
+            };
+          } catch (error) {
+            return {
+              countryName: country.name,
+              capital: country.capital,
+              currentTemp: 'N/A'
+            };
+          }
+        })
+      );
+
+      console.log(currentCitiesTempArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <ApolloProvider client={client}>
