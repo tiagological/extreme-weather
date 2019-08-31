4db5b2274c3a1e041ca8cf7ca7e4cfa1dc9a2757
Tiago <t.marques999@gmail.com>
2019-08-31 14:26:29 +0100

    Removes unused files and GraphQL types

--- schema.js
+++ schema.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 const {
   GraphQLObjectType,
-  GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
   GraphQLFloat,
@@ -20,43 +19,6 @@ const CountryType = new GraphQLObjectType({
     name: { type: GraphQLString },
     alpha2Code: { type: GraphQLString },
     capital: { type: GraphQLString }
-  })
-});
-
-// City Type
-
-const CityKeyType = new GraphQLObjectType({
-  name: 'CityKey',
-  fields: () => ({
-    Key: { type: GraphQLInt }
-  })
-});
-
-// Metric Temp Type
-
-const MetricTempType = new GraphQLObjectType({
-  name: 'MetricTemp',
-  fields: () => ({
-    Value: { type: GraphQLFloat },
-    Unit: { type: GraphQLString }
-  })
-});
-
-// Temp Type
-
-const TempType = new GraphQLObjectType({
-  name: 'Temperature',
-  fields: () => ({
-    Metric: { type: MetricTempType }
-  })
-});
-
-// CityTemp Type
-
-const CityTempType = new GraphQLObjectType({
-  name: 'CityTemp',
-  fields: () => ({
-    Temperature: { type: TempType }
   })
 });
 
@@ -192,56 +154,7 @@ const RootQuery = new GraphQLObjectType({
         }
       }
     },
-    cityTemperature: {
-      type: new GraphQLList(CityTempType),
-      args: {
-        countryCode: { type: GraphQLString },
-        cityName: { type: GraphQLString }
-      },
-      async resolve(parent, args) {
-        const firstResponse = await axios({
-          method: 'get',
-          url: `http://dataservice.accuweather.com/locations/v1/cities/${args.countryCode}/search`,
-          params: {
-            apikey: process.env.ACCUWEATHER_API_KEY,
-            q: args.cityName,
-            language: 'en-gb',
-            details: true
-          }
-        });
 
-        const locationKey = firstResponse.data[0].Key;
-
-        const secondResponse = await axios({
-          method: 'get',
-          url: `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`,
-          params: {
-            apikey: process.env.ACCUWEATHER_API_KEY,
-            language: 'en-gb',
-            details: false
-          }
-        });
-
-        return secondResponse.data;
-      }
-    },
-    cityWeather: {
-      type: new GraphQLList(CityTempType),
-      args: {
-        locationKey: { type: GraphQLInt }
-      },
-      resolve(parents, args) {
-        return axios({
-          method: 'get',
-          url: `http://dataservice.accuweather.com/currentconditions/v1/${args.locationKey}`,
-          params: {
-            apikey: process.env.ACCUWEATHER_API_KEY,
-            language: 'en-gb',
-            details: false
-          }
-        }).then(res => res.data);
-      }
-    },
     lastQuery: {
       type: TimeType,
       resolve() {
