f9ea01c937e284f5a757909bcaf64aa78a129778
Tiago <t.marques999@gmail.com>
2019-08-31 13:37:55 +0100

    Changes log statement for when  GraphQL server is listening

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ app.use(
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`GraphQL Server started on port ${PORT}`));
