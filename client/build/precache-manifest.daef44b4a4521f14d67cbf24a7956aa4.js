87b65e28c5df1dc6ae933228a5a3f1e993d1c302
Tiago Marques <t.marques999@gmail.com>
2022-10-30 15:22:39 +0000

    Migrate to render (#11)
    * chore: Unignore build files
    
    * chore: Update Apollo uri dev value
    
    * chore: Add Procfile
    
    * chore: Create build files
    
    * chore: Migrate to render
    
    * chore: Remove Heroku postbuild script
    
    * chore: User yarn instead of npm
    
    * chore: Use String type for lastFetchedAt property

---
+++ client/build/precache-manifest.daef44b4a4521f14d67cbf24a7956aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258f8b9edce9355ff701b3d6a2dde5d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e34da7049250af3c14ef",
+    "url": "/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "e34da7049250af3c14ef",
+    "url": "/static/js/2.94a93a21.chunk.js"
+  },
+  {
+    "revision": "9fd41b4a1289263d6a5c",
+    "url": "/static/js/main.72f7baa3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfb1a320348fb1f94d1dc2e57817f06d",
+    "url": "/static/media/againts.cfb1a320.otf"
+  }
+]);
